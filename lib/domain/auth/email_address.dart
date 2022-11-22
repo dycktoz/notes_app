b91ebf7561f8607f4f8bd7e434f6f4c0d074a42d
dycktoz <francisco_anza@hotmail.com>
2022-11-22 15:02:02 -0600

    WIP: init

--- lib/domain/auth/email_address.dart
+++ lib/domain/auth/email_address.dart
@@ -1,5 +1,65 @@
+import 'package:flutter/foundation.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class EmailAddress {
   final String value;
+  final Either<Left, Rigth> failure;
 
-  EmailAddress(this.value);
+  factory EmailAddress(String input){
+    assert(input != null )
+    return EmailAddress._(
+      validateEmailAddress(input),
+    );
+  }
+
+  EmailAddress._(this.value, this.failure);
+  @override
+  String toString() => 'EmailAddress(value: $value)';
+
+  @override
+  bool operator ==(covariant EmailAddress other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+  
+
 }
+
+  String validateEmailAddress(String input){
+    const emailRegex =
+      r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
+
+    if (RegExp(emailRegex).hasMatch(input)){
+      return input;
+    } else {
+      throw InvalidEmailException(failedValue: input)
+    }
+  }
+  @freezed
+  abstract class ValueFailure<T> with _$ValueFailure<T>{
+    const factory VAlueFailure.invalidEmail({
+      @required String failedValue, 
+    }) = InvalidEmail<T>;
+  }
+
+  abstract class Failure{
+
+    
+
+  }
+  
+
+  class InvalidEmailFailure implements Failure{
+    final String failedValue;
+    InvalidEmailFailure({@required this.failedValue})
+  }
+  class InvalidPassword implements Failure{
+    final String failedValue;
+    InvalidPassword({@required this.failedValue})
+  }
+
+  
